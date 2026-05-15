5478f26ce3dacb81659a2051ca3ea34fdf3f41a7
KafuiDotse <kafuikwamedotse321@gmail.com>
2026-05-15 23:06:52 +0000

    fix: implemented multiple origin for cors

--- internal/middleware/cors-middleware.go
+++ internal/middleware/cors-middleware.go
@@ -2,9 +2,20 @@ package middleware
 
 import "net/http"
 
+var AllowedOriging = []string{
+	"https://rayaw-store.netlify.app",
+	"localhost:5173",
+}
+
 func CorsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
+		origin := r.Header.Get("Origin")
+		if origin != AllowedOriging[0] && origin != AllowedOriging[1] {
+			http.Error(w, "Origin not allowed", http.StatusForbidden)
+			return
+
+		}
+		w.Header().Set("Access-Control-Allow-Origin", origin)
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
